4b26f1e71448f7ed4e1885d15cc0e6a3a8dafa75
bhgpt <143407920+bhgpt@users.noreply.github.com>
2023-08-28 12:01:38 -0700

    Update SettingDialog.tsx

--- components/Settings/SettingDialog.tsx
+++ components/Settings/SettingDialog.tsx
@@ -63,18 +63,6 @@ export const SettingDialog: FC<Props> = ({ open, onClose }) => {
             aria-hidden="true"
           />
 
-          <div
-            ref={modalRef}
-            className="dark:border-netural-400 inline-block max-h-[400px] transform overflow-y-auto rounded-lg border border-gray-300 bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-[#202123] sm:my-8 sm:max-h-[600px] sm:w-full sm:max-w-lg sm:p-6 sm:align-middle"
-            role="dialog"
-          >
-            <div className="text-lg pb-4 font-bold text-black dark:text-neutral-200">
-              {t('Settings')}
-            </div>
-
-            <div className="text-sm font-bold mb-2 text-black dark:text-neutral-200">
-              {t('Theme')}
-            </div>
 
             <select
               className="w-full cursor-pointer bg-transparent p-2 text-neutral-700 dark:text-neutral-200"
